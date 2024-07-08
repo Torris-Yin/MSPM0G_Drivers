69a42c8775cd4ce09fb5d909dfaf9cfed5a42ff5
Torris Yin <torris.yin@gmail.com>
2024-07-08 09:15:35 +0800

    Add OLED software IIC driver.

---
+++ OLED_Software_IIC/main.c
@@ -0,0 +1,49 @@
+//////////////////////////////////////////////////////////////////////////////////	 
+//  文 件 名   : oled.c
+//  版 本 号   : v2.0
+//  作    者   : Torris
+//  生成日期   : 2024-07-08
+//  最近修改   : 
+//  功能描述   : 0.96寸OLED 接口演示例程(MSPM0G系列)
+//  驱动IC     : SSD1306/SSD1315
+//              说明: 
+//              ----------------------------------------------------------------
+//              GND    电源地
+//              VCC    接3.3v电源
+//              SCL    PB2（时钟）
+//              SDA    PB3（数据）    
+//******************************************************************************/
+#include "ti_msp_dl_config.h"
+#include "oled.h"
+#include "bmp.h"
+
+int main( void )
+{
+    SYSCFG_DL_init();
+    
+    u8 t=' ';
+    OLED_Init();		//初始化OLED
+    while(1) 
+    {		
+        OLED_DrawBMP(0,0,128,64,BMP1);
+        delay_ms(500);
+        OLED_Clear();
+        OLED_ShowChinese(0,0,0,16);//中
+        OLED_ShowChinese(18,0,1,16);//景
+        OLED_ShowChinese(36,0,2,16);//园
+        OLED_ShowChinese(54,0,3,16);//电
+        OLED_ShowChinese(72,0,4,16);//子
+        OLED_ShowChinese(90,0,5,16);//科
+        OLED_ShowChinese(108,0,6,16);//技
+        OLED_ShowString(8,2,(uint8_t *)"ZHONGJINGYUAN",16);
+        OLED_ShowString(20,4,(uint8_t *)"2014/05/01",16);
+        OLED_ShowString(0,6,(uint8_t *)"ASCII:",16);  
+        OLED_ShowString(63,6,(uint8_t *)"CODE:",16);
+        OLED_ShowChar(48,6,t,16);
+        t++;
+        if(t>'~')t=' ';
+        OLED_ShowNum(103,6,t,3,16);
+        delay_ms(500);
+        OLED_Clear();
+    }
+}
---
+++ OLED_Software_IIC/oled.h
@@ -0,0 +1,42 @@
+#ifndef __OLED_H
+#define __OLED_H			  	 
+#include "ti_msp_dl_config.h"
+#define  u8 unsigned char 
+#define  u16 unsigned int
+#define  u32 unsigned int
+#define OLED_CMD  0	//写命令
+#define OLED_DATA 1	//写数据
+
+
+//----------------------------------------------------------------------------------
+//OLED SSD1306 I2C 时钟SCL
+#define		OLED_SCL_Set()			    (DL_GPIO_setPins(GPIO_OLED_PORT, GPIO_OLED_PIN_SCL_PIN))
+#define		OLED_SCL_Clr()				(DL_GPIO_clearPins(GPIO_OLED_PORT, GPIO_OLED_PIN_SCL_PIN))
+
+//----------------------------------------------------------------------------------
+//OLED SSD1306 I2C 数据SDA
+#define		OLED_SDA_Set()				(DL_GPIO_setPins(GPIO_OLED_PORT, GPIO_OLED_PIN_SDA_PIN))
+#define		OLED_SDA_Clr()			    (DL_GPIO_clearPins(GPIO_OLED_PORT, GPIO_OLED_PIN_SDA_PIN))
+				   
+
+//OLED控制用函数
+void delay_ms(unsigned int ms);
+void OLED_ColorTurn(u8 i);
+void OLED_DisplayTurn(u8 i);
+void OLED_WR_Byte(u8 dat,u8 cmd);
+void OLED_Set_Pos(u8 x, u8 y);
+void OLED_Display_On(void);
+void OLED_Display_Off(void);
+void OLED_Clear(void);
+void OLED_ShowChar(u8 x,u8 y,u8 chr,u8 sizey);
+u32 oled_pow(u8 m,u8 n);
+void OLED_ShowNum(u8 x,u8 y,u32 num,u8 len,u8 sizey);
+void OLED_ShowString(u8 x,u8 y,u8 *chr,u8 sizey);
+void OLED_ShowChinese(u8 x,u8 y,u8 no,u8 sizey);
+void OLED_DrawBMP(u8 x,u8 y,u8 sizex, u8 sizey,u8 BMP[]);
+void OLED_Init(void);
+#endif  
+	 
+
+
+
