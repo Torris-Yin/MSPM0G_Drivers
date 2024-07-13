0bb4eabe09562dd40847984c43480a6bbc3d3f9e
Torris Yin <torris.yin@gmail.com>
2024-07-13 11:37:32 +0800

    Modify delay function

--- MPU6050_Hardware_IIC/MPU6050/board.c
+++ MPU6050_Hardware_IIC/MPU6050/board.c
@@ -25,42 +25,14 @@ void board_init(void)
     NVIC_EnableIRQ(I2C_MPU6050_INST_INT_IRQN);
 }
 
-//搭配滴答定时器实现的精确us延时
 void delay_us(unsigned long __us) 
 {
-    uint32_t ticks;
-    uint32_t told, tnow, tcnt = 38;
-
-    // 计算需要的时钟数 = 延迟微秒数 * 每微秒的时钟数
-    ticks = __us * (32000000 / 1000000);
-
-    // 获取当前的SysTick值
-    told = SysTick->VAL;
-
-    while (1)
-    {
-        // 重复刷新获取当前的SysTick值
-        tnow = SysTick->VAL;
-
-        if (tnow != told)
-        {
-            if (tnow < told)
-                tcnt += told - tnow;
-            else
-                tcnt += SysTick->LOAD - tnow + told;
-
-            told = tnow;
-
-            // 如果达到了需要的时钟数，就退出循环
-            if (tcnt >= ticks)
-                break;
-        }
-    }
+    delay_cycles(CPUCLK_FREQ/1000000);
 }
 
 void delay_ms(unsigned long ms) 
 {
-	delay_us( ms * 1000 );
+	delay_cycles(CPUCLK_FREQ/1000);
 }
 
 void delay_1us(unsigned long __us)
